ea9a64a41037503501a0a8ef01d3f4f17f9f3388
(yunjin) <sion11290805@gmail.com>
2022-11-19 22:09:26 +0900

    style: Global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from 'routes';
+import styled from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <S.Container>
+        <RouterProvider router={router} />
+      </S.Container>
+    </QueryClientProvider>
+  );
+};
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    width: 26.25rem;
+    height: 100vh;
+    margin: auto;
+    border: 1px solid black;
+  `,
 };
 
 export default App;
