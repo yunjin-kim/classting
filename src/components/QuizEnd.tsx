1eb4ca54ce23126268e1f80a0d30efe7df6b7d60
(yunjin) <sion11290805@gmail.com>
2022-11-22 20:04:58 +0900

    feat: Title 컴포넌트 추가

--- src/components/QuizEnd.tsx
+++ src/components/QuizEnd.tsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
 
-import Button from './Button';
+import Button from 'components/Button';
+import Title from 'components/Title';
 
 const QuizEnd = () => {
   return (
     <S.Container>
-      <S.Title>퀴즈 끝!</S.Title>
+      <Title>퀴즈 끝!</Title>
       <Link to="/quiz-result">
         <Button backColor={'GREEN'} hoverBackColor={'LIGHT_GREEN'} fontColor={'WHITE'}>
           퀴즈 결과보기
@@ -23,10 +24,6 @@ const S = {
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
-  `,
-
-  Title: styled.h1`
-    text-align: center;
   `,
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Button from 'components/Button';
+import Title from 'components/Title';
 
 const Home = () => {
   return (
     <S.Container>
-      <h1>퀴즈 한번 풀어보시겠어요?</h1>
+      <Title>퀴즈 한번 풀어보시겠어요?</Title>
       <Link to="/quiz">
         <Button backColor={'GREEN'} hoverBackColor={'LIGHT_GREEN'} fontColor={'WHITE'}>
           퀴즈 시작
