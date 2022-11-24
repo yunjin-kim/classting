20d946e556180aa940fe0c3109e8103a0b247a1d
(yunjin) <sion11290805@gmail.com>
2022-11-24 16:33:07 +0900

    feat: provider index.ts로 위치 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,46 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from 'App';
+import { ThemeProvider } from 'styled-components';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
+
+import ErrorBoundary from 'components/Errorboundary';
+import GlobalStyle from 'styles/GlobalStyle';
+import { theme } from 'styles/theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <App />
+            </PersistGate>
+          </Provider>
+        </QueryClientProvider>
+      </ErrorBoundary>
+    </ThemeProvider>
+  </BrowserRouter>,
+);
