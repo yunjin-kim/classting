b5f3dce01169420fdd0452030f6c6548deeda2f0
(yunjin) <sion11290805@gmail.com>
2022-11-24 11:30:41 +0900

    feat: selectQuiz 타입 추가

--- src/types/quiz.ts
+++ src/types/quiz.ts
@@ -12,6 +12,16 @@ export interface QuizType {
   type: QuizKind;
 }
 
+export interface SelectQuizType {
+  category: string;
+  correct_answer: string;
+  selectAnswer: string;
+  difficulty: DifficultyType;
+  incorrect_answers: string[];
+  question: string;
+  type: QuizKind;
+}
+
 type DifficultyType = 'hard' | 'medium' | 'easy';
 
 type QuizKind = 'multiple' | 'boolean';
