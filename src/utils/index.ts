1d4eb010a5e58b5801647de1131ee5ea3388a27e
(yunjin) <sion11290805@gmail.com>
2022-11-24 11:01:03 +0900

    feat: 퀴즈 푸는 시간 변환해주는 유틸 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,14 @@
 export const shuffle = (array: any[]): any[] => {
   return array.sort(() => Math.random() - 0.5);
 };
+
+export const convertTimeFormat = (startTime: number, endTime: number): string => {
+  const time = endTime - startTime;
+  const sec = time / 1000;
+  const min = time / 1000 / 60;
+  const hour = time / 1000 / 60 / 60;
+
+  return `${hour > 1 ? `${Math.floor(hour)}시간` : ''} ${min > 1 ? `${Math.floor(min)}분` : ''} ${
+    sec > 1 ? `${Math.floor(sec)}초` : ''
+  }`;
+};
