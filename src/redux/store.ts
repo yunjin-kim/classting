9d98f1f75692454deb7d62721e2178722662e6d9
(yunjin) <sion11290805@gmail.com>
2022-11-24 11:31:18 +0900

    feat: redux store에 wrongAnswer reducer추가

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,7 @@ import { persistReducer, persistStore } from 'redux-persist';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import quizSolveReducer from 'redux/quiz/quizSolve';
+import wrongAnswersReducer from 'redux/quiz/wrongAnswerNote';
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
@@ -14,6 +15,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   quizSolve: persistReducer(persistConfig, quizSolveReducer),
+  wrongAnswers: persistReducer(persistConfig, wrongAnswersReducer),
 });
 
 export const store = configureStore({
